3f30201372c68aba489df26ba806ffc44e2ea0bd
HoangHieu <dinhhoanghieu321@gmail.com>
2022-04-20 22:25:12 +0700

    Simple UI Home Screen and  Navbar without responsive

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_ui_clone/data/data.dart';
+import 'package:netflix_ui_clone/widgets/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController? _scrollController;
+  double _scrollOffset = 0.0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollController!.offset;
+        });
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print('Cast'),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: CustomAppBar(
+          scrollOffset: _scrollOffset,
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: const [
+          SliverToBoxAdapter(
+            child: ContentHeader(
+              featuredContent: sintelContent,
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(top: 20),
+            sliver: SliverToBoxAdapter(
+              child: Previews(
+                title: 'Previews',
+                contentList: previews,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(bottom: 20),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                title: 'Trending',
+                contentList: trending,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/content_header.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_ui_clone/models/models.dart';
+import 'package:netflix_ui_clone/widgets/widgets.dart';
+
+class ContentHeader extends StatelessWidget {
+  final Content featuredContent;
+  const ContentHeader({Key? key, required this.featuredContent})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: 500,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(featuredContent.imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          height: 500,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.black,
+                Colors.transparent,
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 110,
+          child: SizedBox(
+            width: 250,
+            child: Image.asset(featuredContent.titleImageUrl.toString()),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              VerticalIconButton(
+                icon: Icons.add,
+                title: 'List',
+                onTap: () => print('My List'),
+              ),
+              _PlayButton(),
+              VerticalIconButton(
+                icon: Icons.info_outline,
+                title: 'Info',
+                onTap: () => print('Info'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _PlayButton extends StatelessWidget {
+  const _PlayButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: () => print('Play'),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+        padding: MaterialStateProperty.all<EdgeInsets>(
+          const EdgeInsets.fromLTRB(15, 5, 20, 5),
+        ),
+      ),
+      icon: const Icon(
+        Icons.play_arrow,
+        size: 30,
+      ),
+      label: const Text(
+        'Play',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
